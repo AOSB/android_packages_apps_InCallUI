46fc488ee991f3201903ced1c69980f3a02b0ff4
Lars Greiss <kufikugel@googlemail.com>
2014-07-30 17:51:44 -0300

    InCallUI: Heads up meets SlimKat....SlimKat meets Google IO [2/4] Add native heads up support on dialer.
    We only have show call in background feature. We combine it now with heads up.
    So it is doing exactly same like on Google IO was shown.
    
    Enabled by default.
    
    - Notification is expanded by default
    - Send as requested to force show the heads up no matter what.
    
    Change-Id: Idc1ef59e9821293ac69a72767f918c07acaafc83

--- src/com/android/incallui/InCallPresenter.java
+++ src/com/android/incallui/InCallPresenter.java
@@ -32,6 +32,8 @@ import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.view.IWindowManager;
 import android.content.ActivityNotFoundException;
+import android.os.PowerManager;
+import android.provider.Settings;
 
 import com.android.services.telephony.common.Call;
 import com.android.services.telephony.common.Call.Capabilities;
@@ -805,7 +807,9 @@ public class InCallPresenter implements CallList.Listener {
 
         // check if the user want to have the call UI in background and set it up
         mCallUiInBackground = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.CALL_UI_IN_BACKGROUND, 0) == 1;
+                Settings.System.CALL_UI_IN_BACKGROUND, 1) == 1;
+
+        boolean isHeadsUp = false;
 
         if (mCallUiInBackground) {
             // get power service to check later if screen is on
@@ -819,9 +823,14 @@ public class InCallPresenter implements CallList.Listener {
             } catch (RemoteException e) {
             }
             mCallUiInBackground = pm.isScreenOn() && !isKeyguardShowing;
+
+            // Check if user want to see notification as heads up.
+            isHeadsUp = Settings.System.getInt(mContext.getContentResolver(),
+            Settings.System.CALL_UI_AS_HEADS_UP, 1) == 1;
         }
 
-        mStatusBarNotifier.updateNotificationAndLaunchIncomingCallUi(inCallState, mCallList, mCallUiInBackground);
+        mStatusBarNotifier.updateNotificationAndLaunchIncomingCallUi(
+            inCallState, mCallList, mCallUiInBackground, isHeadsUp);
     }
 
     /**
@@ -833,7 +842,7 @@ public class InCallPresenter implements CallList.Listener {
         // First cancel the actual notification and then update
         mStatusBarNotifier.cancelInCall();
         mStatusBarNotifier.updateNotificationAndLaunchIncomingCallUi(
-                InCallState.INCALL, mCallList, false);
+            InCallState.INCALL, mCallList, false, false);
     }
 
     /**
